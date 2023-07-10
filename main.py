f14bf8df519120ffff8e1a58463cfd5a03fe2d88
Matt Koski <maccam912@gmail.com>
2023-07-09 20:03:03 -0500

    gpt4

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ def get_llm(local: bool = False):
     if not local:
         return ChatOpenAI(
             temperature=0.2,
+            model="gpt-4",
             # openai_api_base="https://local-ai.k3s.koski.co/v1",
             # streaming=True,
             # request_timeout=1800,
