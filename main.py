9ef5187d9d6c1d73b8a8c16ed9825e4f77fbc52f
Matt Koski <maccam912@gmail.com>
2023-07-21 10:24:36 -0500

    gpt4

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def get_llm(model: str = "3.5"):
     elif model == "4":
         return ChatOpenAI(
             temperature=0.2,
-            model="gpt4",
+            model="gpt-4",
             # openai_api_base="https://local-ai.k3s.koski.co/v1",
             # streaming=True,
             # request_timeout=1800,
